42815ae14328a8179a2dcd9f6a2eb441454cdc89
HiAndi <longandi@163.com>
2017-03-30 23:37:09 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -16,7 +16,7 @@ $(function () {
     gardenCtx.globalCompositeOperation = "lighter";
     garden = new Garden(gardenCtx, gardenCanvas);
 	
-	$("#content").css("width", $loveHeart.width() + $("#code").width() + 100);
+	$("#content").css("width", $loveHeart.width() + $("#code").width());
 	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
 	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
 	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
@@ -116,7 +116,7 @@ function timeElapse(date){
 
 function showMessages() {
 	adjustWordsPosition();
-	$('#messages').fadeIn(50000, function() {
+	$('#messages').fadeIn(5000, function() {
 		showLoveU();
 	});
 }
